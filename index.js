4015abcab876c27a99b3896767b5eddd71f6187b
Akhilesh Pandey <akhilesh.cbsereg@gmail.com>
2018-07-21 20:01:06 +0530

    Updated btn color

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 window.onscroll = function() {myFunction()};
 
 // Get the header
-var header = document.getElementById("myHeader");
+var header = document.getElementById('header1');
 
 // Get the offset position of the navbar
 var sticky = header.offsetTop;
