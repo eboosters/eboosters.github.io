4b42f686a6a7bb32831477e5a567dc6b4c9b7c9d
Akhilesh Pandey <akhilesh.cbsereg@gmail.com>
2018-07-25 13:38:00 +0530

    Added About page with slider section

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Sticky header
 // When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
-
+var url=location.pathname;
+var header     = document.getElementById('header1');
+var sticky = header.offsetTop;
+var background = document.querySelector('.background');
 // Get the header
-var header = document.getElementById('header1');
 
-// Get the offset position of the navbar
 var sticky = header.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
@@ -16,3 +16,29 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+function scroll() {
+  if (window.pageYOffset > 60) {
+    background.style.opacity=1;
+    var cap=document.querySelectorAll(".capt");
+    cap.forEach(function(data){
+    	data.style.color="#fff";
+    });
+}}
+// Scroll down button function
+$(document).ready(function() {
+    $("#slider").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 500 }, "slow");
+        return false;
+    });
+
+});
+if(url.split('/')[6].split('.')[0]==="index"){
+window.onscroll = function() {myFunction();scroll();};
+}
+else{
+  header.classList.add("sticky");
+}
+
+
+// 
